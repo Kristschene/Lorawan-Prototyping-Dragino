652032e741b0e097b53ab12d82767efc7ac00561
Kristschene <43150691+Kristschene@users.noreply.github.com>
2020-02-13 13:54:57 +0100

    Update Lora-Dragino-Prototyping.ino
    Code cleaning

--- Lora-Dragino-Prototyping.ino
+++ Lora-Dragino-Prototyping.ino
@@ -1,56 +1,52 @@
-
 //Temp.&Feuchte Sensor DHT11
-/*#include <Adafruit_Sensor.h>
-#include <DHT.h>*/
+
+/*--------------------------------------- INCLUDES --------------------------------------------- */
+// Include library for Temp/Humidity sensor
 #include <DHT_U.h>
 
-//Verbindungen TTN & Cayenne
+// Include library for LORAWAN connection
 #include <lmic.h>
+
+// Include library for Cayenne Cloud which is used to prepare payloads in correct form 
 #include <CayenneLPP.h>
+
+// Include hardware abstraction layer for arduino environment
 #include <hal/hal.h>
 
 
 
-
-//DEFINES
-
-//Temp/Feuchte
+/*---------------------------------------- DEFINES ---------------------------------------------- */
+// Temp/Humidity sensor
 #define DHTPIN 5           // Digital pin connected to the DHT sensor 
-#define DHTTYPE    DHT11     // DHT 11  // Es handelt sich um den DHT11 Sensor
-DHT_Unified dht(DHTPIN, DHTTYPE); //Der Sensor wird ab jetzt mit „dth“ angesprochen
-
-uint32_t delayMS;
-sensor_t sensor;
+#define DHTTYPE    DHT11   // Type DHT 11 is used as sensor
 
 
 
-// Cayenne--------------------
-CayenneLPP lpp(51);
-
-#ifdef CREDENTIALS
-static const u1_t NWKSKEY[16] = NWKSKEY1;
-static const u1_t APPSKEY[16] = APPSKEY1;
-static const u4_t DEVADDR = DEVADDR1;
-#else
+/*------------------------------------ GLOBAL VARIABLES ----------------------------------------- */
+// Credentials which are necessary to connect to TTN (modify if you use other TTN account or device)
 static const u1_t NWKSKEY[16] = { 0x76, 0x99, 0xE0, 0x63, 0x90, 0x0A, 0x93, 0x3F, 0x7D, 0x72, 0x8D, 0x7E, 0xE4, 0x5C, 0x5B, 0x07 };
 static const u1_t APPSKEY[16] = { 0xA3, 0xD2, 0x85, 0xB8, 0x09, 0xBD, 0x80, 0xDE, 0xD4, 0x85, 0x47, 0x8E, 0xF2, 0xAA, 0x5B, 0xF7 };
 static const u4_t DEVADDR = 0x260117B6 ;
-#endif
 
-// These callbacks are only used in over-the-air activation, so they are
-// left empty here (we cannot leave them out completely unless
-// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
-void os_getArtEui (u1_t* buf) { }
-void os_getDevEui (u1_t* buf) { }
-void os_getDevKey (u1_t* buf) { }
-
-static osjob_t sendjob;
-
-// Schedule TX every this many seconds (might become longer due to duty
-// cycle limitations).
+// Schedule TX messages over Lora within defined seconds
 const unsigned TX_INTERVAL = 150; 
 
-// Pin mapping Dragino Shield
+// Delay which is minimal needed between sensor readings
+uint32_t delayMS;
+
+// Instantiate object for Temp/Humidity sensor class
+DHT_Unified dht(DHTPIN, DHTTYPE); 
+
+// Instantiate object for DHT sensor which is used to read out infos from library
+sensor_t sensor;
+
+// Instantiate Cayenne format object with maximum of 51 bytes lenght
+CayenneLPP lpp(51);
+
+// Instantiate sendjob object from LMIC library
+static osjob_t sendjob;
+
+// Declare struct for Dragino Shield pin mapping 
 const lmic_pinmap lmic_pins = {
     .nss = 10,
     .rxtx = LMIC_UNUSED_PIN,
@@ -58,13 +54,38 @@ const lmic_pinmap lmic_pins = {
     .dio = {2, 6, 7},
 };
 
+
+
+/*-------------------------------------- PROTOTYPES ------------------------------------------- */
+
+
+
+
+/*--------------------------------------- CALLBACKS -------------------------------------------- */
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+
+
+/*--------------------------------------- FUNCTIONS -------------------------------------------- */
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Setup - will be processed once at the beginning
+///////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
+	
+	// Initialize serial interface with 9600 Baud-Rate
     Serial.begin(9600);
     Serial.println(F("Starting..."));
-     //Temp/Feuchte
-   
+	
+	// Start Temp/Humidity sensor
     dht.begin(); //DHT11 Sensor starten
     
+	// Get all data of temperature sensor and send it over the serial interface
     dht.temperature().getSensor(&sensor);
     Serial.println(F("------------------------------------"));
     Serial.println(F("Temperature Sensor"));
@@ -75,7 +96,9 @@ void setup() {
     Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
     Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
     Serial.println(F("------------------------------------"));
-    // Print humidity sensor details.
+	
+	
+    // Get all data of humidity sensor and send it over the serial interface
     dht.humidity().getSensor(&sensor);
     Serial.println(F("Humidity Sensor"));
     Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
@@ -86,10 +109,11 @@ void setup() {
     Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
     Serial.println(F("------------------------------------"));
     
+	
     // Set delay between sensor readings based on sensor details.
     delayMS = sensor.min_delay / 1000;
   
-    // LMIC init
+    // Initialize LMIC lib
     os_init();
 
     // Reset the MAC state. Session and pending data transfers will be discarded.
@@ -107,70 +131,88 @@ void setup() {
     // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
     LMIC_setDrTxpow(DR_SF12,14);
 
-    // Start job
+    // Start job manually once; afterwards it will be started every defined seconds (see variable TX_INTERVAL )
     do_send(&sendjob);
 }
 
-//**************************************************
 
-
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Loop will be processed after setup for infinite times
+///////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {   
-    os_runloop_once();      //lmic
 
+	// Let LMIC run in loop
+    os_runloop_once();      
 }
 
-//'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// onEvent is Handler which is called every time a event occurs in the LMIC stack 
+///////////////////////////////////////////////////////////////////////////////////////////////////
 void onEvent (ev_t ev) {
+	
+	// If the event was trigger by completing a sendjob 
     if (ev == EV_TXCOMPLETE) {
+		
         Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+		
         // Schedule next transmission
         os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
     }
 }
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// do_send is called manually or automatically by the LMIC OS every time a timer elapses TX_INTERVAL seconds
+///////////////////////////////////////////////////////////////////////////////////////////////////
 void do_send(osjob_t* j){
-    //Payload to send (uplink)
-    //static uint8_t i = 0;
-    //i++;
-    //static uint8_t message[] = i;
+	// Instantiate float variables for temp and humidity value
     float temperature;
     float humidity;
 
-    // Get temperature event and print its value.
+    // Instantiate sensor event object
     sensors_event_t event;
 
-    // Delay between measurements.
-  delay(delayMS);
+    // Minimum delay between measurements.
+	delay(delayMS);
   
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
-  }
-  else {
-    Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
-    Serial.println(F("°C"));
-    temperature = event.temperature;
-    
-  }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
-  }
-  else {
-    Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
-    humidity = event.relative_humidity;
-  }
+  
+	// Get temperature data 
+	dht.temperature().getEvent(&event);
+	if (isnan(event.temperature)) 
+	{
+		Serial.println(F("Error reading temperature!"));
+	}
+	else 
+	{
+		Serial.print(F("Temperature: "));
+		Serial.print(event.temperature);
+		Serial.println(F("°C"));
+		temperature = event.temperature;
+	}
+	
+	
+	// Get humidity data
+	dht.humidity().getEvent(&event);
+	if (isnan(event.relative_humidity)) 
+	{
+		Serial.println(F("Error reading humidity!"));
+	}
+	else 
+	{
+		Serial.print(F("Humidity: "));
+		Serial.print(event.relative_humidity);
+		Serial.println(F("%"));
+		humidity = event.relative_humidity;
+	}
    
     // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & OP_TXRXPEND) {
+    if (LMIC.opmode & OP_TXRXPEND) 
+	{
         Serial.println(F("OP_TXRXPEND, not sending"));
-    } else {
+    } 
+	else 
+	{
         // Prepare upstream data transmission at the next possible time.
         lpp.reset();
         lpp.addTemperature(1, temperature);
